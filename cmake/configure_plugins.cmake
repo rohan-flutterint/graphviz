70726a46fb8427fc8d0ad106d33eee6eb4a467f4
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-11-25 10:18:52 -0800

    XXX try host system

--- cmake/configure_plugins.cmake
+++ cmake/configure_plugins.cmake
@@ -30,8 +30,10 @@ set(ROOT $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX})
 # logic from the root CMakeLists.txt for finding the library directory does not
 # propagate to here, so we need to repeat discovery
 execute_process(
+  COMMAND echo "CMAKE_HOST_SYSTEM_NAME is ${CMAKE_HOST_SYSTEM_NAME}")
+execute_process(
   COMMAND echo "CMAKE_SYSTEM_NAME is ${CMAKE_SYSTEM_NAME}")
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+if(${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux")
   include(GNUInstallDirs)
   set(LIBRARY_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}")
 else()
